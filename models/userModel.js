96f38e055f52db5975c5191c3e4c3004eb7a93e1
srishtayy <srishtidhir652@gmail.com>
2024-02-14 16:34:20 +0530

    Password encryption done

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator')
+const bcrypt = require('bcryptjs');
+
 const userSchema = new mongoose.Schema({
     name: {
         type: 'String',
@@ -28,7 +30,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Please confirm your password'],
         validate: {
-            // This only works on CREATE and SAVE!!!
+            // This only works on CREATE and SAVE!!! .create() or .save()
             validator: function (el) {
                 return el === this.password;
             },
@@ -43,6 +45,18 @@ const userSchema = new mongoose.Schema({
         default: true,
         select: false
     }
+});
+//betweeen getting data and saving it we will manipulate/encrypt data
+userSchema.pre('save', async function (next) {
+    // Only run this function if password was actually modified(we only want to modify password if it is changed)
+    if (!this.isModified('password')) return next();
+
+    // Hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+
+    // Delete passwordConfirm field
+    this.passwordConfirm = undefined;
+    next();
 });
 const User = mongoose.model('User', userSchema);
 
