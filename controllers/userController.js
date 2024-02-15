61e8866b2a5c3a74ab1855e6644648ba8e4dcd5a
srishtayy <srishtidhir652@gmail.com>
2024-02-15 15:59:11 +0530

    Implemented login function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,18 @@
-const fs = require('fs')
-const express = require('express')
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
 
-exports.getAllUsers = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route not yet defned'
-    })
-}
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+
+    const users = await User.find();
+
+    res.status(200).json({
+        status: 'success',
+        results: users.length,
+        data: {
+            users
+        }
+    });
+});
 exports.getUser = (req, res) => {
     res.status(500).json({
         status: 'error',
