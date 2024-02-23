cfb4e20d023d04dee630001a4efe00f6fd8d1440
srishtayy <srishtidhir652@gmail.com>
2024-02-23 13:44:08 +0530

    Implemented create and get Reviews

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
+const reviewRouter = require('./routes/reviewRoutes');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const xss = require('xss-clean');
@@ -68,6 +69,7 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 app.all('*', (req, res, next) => {
 
